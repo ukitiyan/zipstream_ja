7be13cd91def732898bba4893123d0c8f1e882f0
TAKAHASHI Yuki <y-takahashi@begood-tech.com>
2014-11-10 12:20:40 +0900

    first commit

---
+++ appinfo/app.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * ownCloud - zipstream_ja
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Begood Technology Corp. <y-takahashi@begood-tech.com>
+ * @copyright Begood Technology Corp. 2014
+ */
+
+namespace OCA\Zipstream_ja\AppInfo;
+
+\OC::$CLASSPATH['OC_Files'] = 'zipstream_ja/lib/files.php';
